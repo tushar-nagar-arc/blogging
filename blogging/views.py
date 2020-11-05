5ae684af2af8908e77539bc20f2f5f768afb9ff5
tushar <tusharnagar8700@gmail.com>
2020-11-05 12:44:24 +0530

    Initial Commit

---
+++ blogging/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import DetailView,ListView
+# Create your views here.
+class BlogListView(ListView):
+    model=Post
+    template_name='home.html'
+class BlogDetailView(DetailView):
+    model=Post
+    template_name='post_detail.html'
